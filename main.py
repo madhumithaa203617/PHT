b2efd7dd6d1d7f9da8be7e96e87bd787870ea32a
madhumitha203617 <24uad153madhu@kgkite.ac.in>
2024-12-08 22:49:53 +0200

    1ST COMMIT

--- main.py
+++ main.py
@@ -1,14 +1,23 @@
 def calculate_average(numbers):
     """Calculate the average of a list of numbers."""
-    total = 0
+    total=0
     for num in numbers:
-        total += num
-    average = total / len(numbers) 
+         total+=num
+         average = total / len(numbers) 
     return average
 
 def main():
-    numbers = [] 
-    print("The average is:", calculate_average(numbers))
+    numbers = []
+
+    n=int(input("Enter the total length of the list:"))
+    for i in range(1,n+1):
+        count=1
+        a=int(input("Enter the element" ))
+        print("The",count,"element is",a)
+
+        numbers.append(a)
+        count+=1
+    print("The average is:",calculate_average(numbers))
 
 if __name__ == "__main__":
     main()
